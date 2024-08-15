bc6012d34912201e544e33b256263a9780416abc
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-08-15 12:53:09 +0600

    add triangle calculation

---
+++ scripts/triangle-calculation.js
@@ -0,0 +1,18 @@
+function calculateTraiangleArea() {
+    // get triangle base value
+const triangleBaseInput = document.getElementById('triangle-base');
+const triangleBaseText = triangleBaseInput.value;
+const base = parseFloat(triangleBaseText);
+console.log(base)
+
+// get triangle height value 
+const triangleHeightInput = document.getElementById('triangle-height');
+const triangleHeightText = triangleHeightInput.value;
+const height = parseFloat(triangleHeightText);
+ console.log(height);
+
+ // calculate triangle area 
+ const area = 0.5 * base * height;
+ console.log(area);
+ 
+}

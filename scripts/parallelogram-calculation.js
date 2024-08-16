aafb0dbe2e4439b181e9f0c43300240c035e1a7a
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-08-16 21:01:38 +0600

    add reactangle  calculation

---
+++ scripts/parallelogram-calculation.js
@@ -0,0 +1,14 @@
+function calculateParallelogramArea() {
+    const parallelogramBaseArea = document.getElementById('parallelogram-base');
+    const parallelogramBaseText = parallelogramBaseArea.value;
+    const base = parseFloat(parallelogramBaseText);
+    console.log(base);
+
+    const parallelogrameLengthArea  = document.getElementById('parallelogram-length');
+    const parallelogrameLengthText = parallelogrameLengthArea.value;
+    const length = parseFloat(parallelogrameLengthText);
+    console.log(length);
+
+    //get display parallelograme area`
+
+}

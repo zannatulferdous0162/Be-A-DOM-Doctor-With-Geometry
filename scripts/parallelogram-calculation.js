31a959bb37613439548c91178814ad95bc254175
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-08-17 14:18:00 +0600

    add reactangle  calculation

--- scripts/parallelogram-calculation.js
+++ scripts/parallelogram-calculation.js
@@ -1,14 +1,40 @@
-function calculateParallelogramArea() {
-    const parallelogramBaseArea = document.getElementById('parallelogram-base');
-    const parallelogramBaseText = parallelogramBaseArea.value;
-    const base = parseFloat(parallelogramBaseText);
-    console.log(base);
-
-    const parallelogrameLengthArea  = document.getElementById('parallelogram-length');
-    const parallelogrameLengthText = parallelogrameLengthArea.value;
-    const length = parseFloat(parallelogrameLengthText);
-    console.log(length);
-
-    //get display parallelograme area`
-
+function calculateParallelogramArea (){
+   const base = getInputValueById('parallelogram-base');
+   const height = getInputValueById('parallelogram-height')
+ 
+   const area = base * height ;
+   console.log(area)
+   
 }
+
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputText = inputField.value;
+    const inputValue = parseFloat(inputText);
+    return inputValue;
+}
+
+
+
+
+
+// function calculateParallelogramArea() {
+//     const parallelogramBaseArea = document.getElementById('parallelogram-base');
+//     const parallelogramBaseText = parallelogramBaseArea.value;
+//     const base = parseFloat(parallelogramBaseText);
+//     console.log(base);
+
+//     const parallelogrameLengthArea  = document.getElementById('parallelogram-length');
+//     const parallelogrameLengthText = parallelogrameLengthArea.value;
+//     const length = parseFloat(parallelogrameLengthText);
+//     console.log(length);
+
+//     //calculation  parallelograme
+//     const area  = base * length;
+//     console.log(area);
+
+//     //get display parallelograme area
+//     const displayParallelogramArea = document.getElementById('parallelogram-area');
+//     displayParallelogramArea.innerText = area;
+
+// }

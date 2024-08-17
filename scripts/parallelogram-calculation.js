812af56a82812bbc225a5bc1fcf86bd57f54eed4
zannatulferdous0162 <zannatulferdous0162@gmail.com>
2024-08-17 14:25:18 +0600

    add reactangle  calculation

--- scripts/parallelogram-calculation.js
+++ scripts/parallelogram-calculation.js
@@ -1,40 +1,24 @@
-function calculateParallelogramArea (){
-   const base = getInputValueById('parallelogram-base');
-   const height = getInputValueById('parallelogram-height')
- 
-   const area = base * height ;
-   console.log(area)
-   
+function calculateParallelogramArea() {
+    const base = getInputValueById('parallelogram-base');
+    const height = getInputValueById('parallelogram-height')
+
+    const area = base * height;
+    console.log(area)
+
+    //display
+    setInnerTextById('parallelogram-area',area)
+
+
 }
 
-function getInputValueById(inputFieldId){
+function getInputValueById(inputFieldId) {
     const inputField = document.getElementById(inputFieldId);
     const inputText = inputField.value;
     const inputValue = parseFloat(inputText);
     return inputValue;
 }
 
-
-
-
-
-// function calculateParallelogramArea() {
-//     const parallelogramBaseArea = document.getElementById('parallelogram-base');
-//     const parallelogramBaseText = parallelogramBaseArea.value;
-//     const base = parseFloat(parallelogramBaseText);
-//     console.log(base);
-
-//     const parallelogrameLengthArea  = document.getElementById('parallelogram-length');
-//     const parallelogrameLengthText = parallelogrameLengthArea.value;
-//     const length = parseFloat(parallelogrameLengthText);
-//     console.log(length);
-
-//     //calculation  parallelograme
-//     const area  = base * length;
-//     console.log(area);
-
-//     //get display parallelograme area
-//     const displayParallelogramArea = document.getElementById('parallelogram-area');
-//     displayParallelogramArea.innerText = area;
-
-// }
+function setInnerTextById(elementId, area) {
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
